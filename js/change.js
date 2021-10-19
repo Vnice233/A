316aee1f9f977f1c7740341d92ebc1a9fa986891
Vnice233 <92745526+Vnice233@users.noreply.github.com>
2021-10-19 18:45:08 +0800

    Update change.js

--- js/change.js
+++ js/change.js
@@ -1,38 +1,87 @@
 var t=[1,0,0,0,0,0,0,0,0,0];
-var w,r,test=1;
+var w,r,i=1,test=1;
 var n=0;
 function Win(){
-  if (t[1]==1&&t[2]==1&&t[3]==1) alert("you win!");
-  if (t[1]==1&&t[4]==1&&t[7]==1) alert("you win!");
-  if (t[1]==1&&t[5]==1&&t[9]==1) alert("you win!");
-  if (t[2]==1&&t[5]==1&&t[8]==1) alert("you win!");
-  if (t[3]==1&&t[6]==1&&t[9]==1) alert("you win!");
-  if (t[3]==1&&t[5]==1&&t[7]==1) alert("you win!");
-  if (t[4]==1&&t[5]==1&&t[6]==1) alert("you win!");
-  if (t[7]==1&&t[8]==1&&t[9]==1) alert("you win!");
+  if (t[1]==1&&t[2]==1&&t[3]==1) {alert("你赢了！");t[0]=3;}
+  else if (t[1]==1&&t[4]==1&&t[7]==1) {
+    alert("你赢了！");
+    t[0]=3;
+  }
+  else if (t[1]==1&&t[5]==1&&t[9]==1) {
+    alert("你赢了！");
+    t[0]=3;
+  }
+  else if (t[2]==1&&t[5]==1&&t[8]==1) {
+  alert("你赢了！");
+  t[0]=3;
+  }
+  else if (t[3]==1&&t[6]==1&&t[9]==1) {
+    alert("你赢了！");
+    t[0]=3;}
+  else if (t[3]==1&&t[5]==1&&t[7]==1) {
+    alert("你赢了！");
+    t[0]=3;}
+  else if (t[4]==1&&t[5]==1&&t[6]==1) {
+    alert("你赢了！");
+    t[0]=3;}
+  else if (t[7]==1&&t[8]==1&&t[9]==1) {
+    alert("你赢了！");
+    t[0]=3;}
 }
 function Lose(){
-  if (t[1]==2&&t[2]==2&&t[3]==2) alert("you Lose!");
-  if (t[1]==2&&t[4]==2&&t[7]==2) alert("you Lose!");
-  if (t[1]==2&&t[5]==2&&t[9]==2) alert("you Lose!");
-  if (t[2]==2&&t[5]==2&&t[8]==2) alert("you Lose!");
-  if (t[3]==2&&t[6]==2&&t[9]==2) alert("you Lose!");
-  if (t[3]==2&&t[5]==2&&t[7]==2) alert("you Lose!");
-  if (t[4]==2&&t[5]==2&&t[6]==2) alert("you Lose!");
-  if (t[7]==2&&t[8]==2&&t[9]==2) alert("you Lose!");
+  if (t[1]==2&&t[2]==2&&t[3]==2) {
+    alert("你输了！");
+    t[0]=3;
+  }
+  else if (t[1]==2&&t[4]==2&&t[7]==2) {
+    alert("你输了！");
+    t[0]=3;
+  }
+  else if (t[1]==2&&t[5]==2&&t[9]==2) {
+    alert("你输了！");
+    t[0]=3;
+  }
+  else if (t[2]==2&&t[5]==2&&t[8]==2) {
+    alert("你输了！");
+    t[0]=3;
+  }
+  else if (t[3]==2&&t[6]==2&&t[9]==2) {
+    alert("你输了！");
+    t[0]=3;
+  }
+  else if (t[3]==2&&t[5]==2&&t[7]==2) {
+    alert("你输了！");
+    t[0]=3;
+  }
+  else if (t[4]==2&&t[5]==2&&t[6]==2) {
+    alert("你输了！");
+    t[0]=3;
+  }
+  else if (t[7]==2&&t[8]==2&&t[9]==2) {
+    alert("你输了！");
+    t[0]=3;
+  }
+}
+function Re(){
+  t[0]=1;
+  for (i=1;i<=9;i++){
+  w="t"+i;
+  document.getElementById(w).style.background = "rgba(0, 0, 0, 0.5)";
+  t[i]=0;
+}
 }
 function T1() {
    word ="t";
-   if (t[1] == 0) {
+   if ((t[1] == 0)&&(t[0]=1)){
      document.getElementById("t1").style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/X.png)";
      t[1]=1;
      Win();
      ++n;
-     if (n==9) alert("No winner!");
+     if ((n==9)&&(t[0]==1)) alert("平局！");
      if(n!=9){
      while(1){
       r=Math.floor(Math.random()*9);
-      if(t[r] == 0)
+      if((t[r] == 0)&&(t[0]==1))
       {
       w="t"+r;
       document.getElementById(w).style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/O.png)"
@@ -46,16 +95,16 @@ function T1() {
 }
 function T2() {
   word ="t";
-  if (t[2] == 0) {
+  if ((t[2] == 0)&&(t[0]=1)){
     document.getElementById("t2").style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/X.png)";
     t[2]=1;
     ++n;
     Win();
-    if (n==9) alert("No winner!");
+    if ((n==9)&&(t[0]==1)) alert("平局！");
     if(n!=9){
     while(1){
     r=Math.floor(Math.random()*9);
-     if(t[r]==0)
+     if((t[r]==0)&&(t[0]==1))
      {
      w="t"+r;
      document.getElementById(w).style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/O.png)"
@@ -69,16 +118,16 @@ function T2() {
 }
 function T3() {
   word ="t";
-  if (t[3] == 0) {
+  if ((t[3] == 0)&&(t[0]=1)) {
     document.getElementById("t3").style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/X.png)";
     t[3]=1;
     ++n;
     Win();
-    if (n==9) alert("No winner!");
+    if ((n==9)&&(t[0]==1)) alert("平局！");
     if(n!=9){
     while(1){
     r=Math.floor(Math.random()*9);
-     if(t[r]==0)
+     if((t[r]==0)&&(t[0]==1))
      {
      w="t"+r;
      document.getElementById(w).style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/O.png)"
@@ -92,16 +141,16 @@ function T3() {
 }
 function T4() {
   word ="t";
-  if (t[4] == 0) {
+  if ((t[4] == 0)&&(t[0]=1)) {
     document.getElementById("t4").style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/X.png)";
     t[4]=1;
     ++n;
     Win();
-    if (n==9) alert("No winner!");
+    if ((n==9)&&(t[0]==1)) alert("平局！");
     if(n!=9){
     while(1){
     r=Math.floor(Math.random()*9);
-     if(t[r]==0)
+     if((t[r]==0)&&(t[0]==1))
      {
      w="t"+r;
      document.getElementById(w).style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/O.png)"
@@ -116,16 +165,16 @@ function T4() {
 function T5() {
 
   word ="t";
-  if (t[5] == 0) {
+  if ((t[5] == 0)&&(t[0]=1)) {
     document.getElementById("t5").style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/X.png)";
     t[5]=1;
     ++n;
     Win();
-    if (n==9) alert("No winner!");
+    if ((n==9)&&(t[0]==1)) alert("平局！");
     if(n!=9){
     while(1){
     r=Math.floor(Math.random()*9);
-     if(t[r]==0)
+     if((t[r]==0)&&(t[0]==1))
      {
      w="t"+r;
      document.getElementById(w).style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/O.png)"
@@ -140,16 +189,16 @@ function T5() {
 function T6() {
 
   word ="t";
-  if (t[6] == 0) {
+  if ((t[6] == 0)&&(t[0]=1)) {
     document.getElementById("t6").style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/X.png)";
     t[6]=1;
     ++n;
     Win();
-    if (n==9) alert("No winner!");
+    if ((n==9)&&(t[0]==1)) alert("平局！");
     if(n!=9){
     while(1){
     r=Math.floor(Math.random()*9);
-     if(t[r]==0)
+     if((t[r]==0)&&(t[0]==1))
      {
      w="t"+r;
      document.getElementById(w).style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/O.png)"
@@ -164,16 +213,16 @@ function T6() {
 function T7() {
 
   word ="t";
-  if (t[7] == 0) {
+  if ((t[7] == 0)&&(t[0]=1)) {
     document.getElementById("t7").style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/X.png)";
     t[7]=1;
     ++n;
     Win();
-    if (n==9) alert("No winner!");
+    if ((n==9)&&(t[0]==1)) alert("平局！");
     if(n!=9){
     while(1){
     r=Math.floor(Math.random()*9);
-     if(t[r]==0)
+     if((t[r]==0)&&(t[0]==1))
      {
      w="t"+r;
      document.getElementById(w).style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/O.png)"
@@ -188,7 +237,7 @@ function T7() {
 function T8() {
 
   word ="t";
-  if (t[8] == 0) {
+  if ((t[8] == 0)&&(t[0]=1)) {
     document.getElementById("t8").style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/X.png)";
     t[8]=1;
     ++n;
@@ -196,7 +245,7 @@ function T8() {
     if(n!=9){
     while(1){
     r=Math.floor(Math.random()*9);
-     if(t[r]==0)
+     if((t[r]==0)&&(t[0]==1))
      {
      w="t"+r;
      document.getElementById(w).style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/O.png)"
@@ -211,7 +260,7 @@ function T8() {
 function T9() {
 
   word ="t";
-  if (t[9] == 0) {
+  if ((t[9] == 0)&&(t[0]=1)) {
     document.getElementById("t9").style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/X.png)";
     t[9]=1;
     ++n;
@@ -219,7 +268,7 @@ function T9() {
     if(n!=9){
     while(1){
     r=Math.floor(Math.random()*9);
-     if(t[r]==0)
+     if((t[r]==0)&&(t[0]==1))
      {
      w="t"+r;
      document.getElementById(w).style.background ="url(https://cdn.jsdelivr.net/gh/Vnice233/Vnice.github.io/img/O.png)"
